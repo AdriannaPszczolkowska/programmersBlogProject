d16173d73d7c0f198fb0d6183e4b8678cb06d28b
AdrainnaPszczolkowska <adrianna.pszczolkowska@gmail.com>
2021-06-13 16:44:03 +0200

    added image filed to Post model

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,10 @@ from django.db import models
 # Create your models here.
 # title, text, create Date
 class Post(models.Model):
-    postId = models.AutoField
     title = models.CharField(max_length = 200)
     text = models.TextField()
     createDate = models.DateField(auto_now_add =True)
-    #image = models.ImageField(upload_to ='imagies/', blank =True)
+    image = models.ImageField(upload_to ='images/', blank = True)
 
     def __str__(self):
         return self.title + " " + str(self.createDate)
